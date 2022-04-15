104f79981b9aee031ee2888d58c86f76f3c2a6d8
Lucas Oliveira Dias Marques Ferreira <100947807+TinierClover041@users.noreply.github.com>
2022-04-15 14:57:16 -0300

    Update string.py

--- string.py
+++ string.py
@@ -1,5 +1,10 @@
-#programa que lê uma string e devolve ela inverida
-s=input("Digite a mensagem a ser invertida:")#string inicial
+#programa para saber se uma palavra é Palíndromo
+#se a mensagem for devolvida da mesma forma que foi digitada pelo usuário significa que éum Palíndromo
+s=input("Digite a mensagem a ser verificada:")#string inicial
 stringlenght=len (s) #Calcula o comprimento da lista
 slicedString=s [stringlenght::-1]# fatiando
 print(slicedString) #imprime a string invertida
+
+
+print("Se a mensagem for devolvida da mesma maneira que o usuário digitou,significa que ela é um Palíndromo")
+
